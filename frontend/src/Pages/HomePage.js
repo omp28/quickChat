fe3184379bd696772071d1549663785a1a596d91
om patel <om28patel2004@gmail.com>
2024-03-10 20:29:58 +0530

    feat:login and signup page

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -1,7 +1,51 @@
 import React from "react";
-
+import {
+  Box,
+  Container,
+  Text,
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+} from "@chakra-ui/react";
+import Login from "../components/Authentication/Login";
+import Signup from "../components/Authentication/Signup";
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <Box className="bg-black h-screen w-screen">
+      <Container w="100%" maxW="xl" className=" pt-20 text-white">
+        <Box
+          d="flex"
+          textAlign="center"
+          justifyContent="center"
+          w="100%"
+          p={4}
+          m="0 0 15px 0"
+          borderRadius="lg"
+          borderWidth="2px"
+        >
+          <Text fontSize="3xl">quickChat</Text>
+        </Box>
+        <Box>
+          <Tabs variant="soft-rounded" colorScheme="gray">
+            <TabList>
+              <Tab>Tab 1</Tab>
+              <Tab>Tab 2</Tab>
+            </TabList>
+            <TabPanels>
+              <TabPanel>
+                <Login />
+              </TabPanel>
+              <TabPanel>
+                <Signup />
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
+        </Box>
+      </Container>
+    </Box>
+  );
 };
 
 export default HomePage;
