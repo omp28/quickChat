24f76db5473ea83c3bd1de656298b53a32efd718
om patel <om28patel2004@gmail.com>
2024-03-10 06:46:37 +0530

    feat(routing):client side navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./Pages/HomePage";
+import ChatPage from "./Pages/ChatPage";
 
 function App() {
   return (
-    <div className="App">
-      <h1> App </h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} exact />
+        <Route path="/chat" element={<ChatPage />} />
+      </Routes>
+    </Router>
   );
 }
 
