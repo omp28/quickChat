32f90026ad5b24533fe7ac16a3d6c57c9360d456
om patel <om28patel2004@gmail.com>
2024-03-12 00:07:13 +0530

    feat:password encrypt and authentication
    feat:server side user authentication
    feat:store password in encrypted form using bcrypt

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -36,4 +36,21 @@ const registerUser = expressAsyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { registerUser };
+const authUser = expressAsyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      pic: user.pic,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
+});
+
+module.exports = { registerUser, authUser };
