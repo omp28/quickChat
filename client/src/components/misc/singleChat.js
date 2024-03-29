57d78ce21ec267a6c6b5f786bc4e7d3e4ff76f0d
om patel <om28patel2004@gmail.com>
2024-03-29 10:09:59 +0530

    implement socket.io

--- client/src/components/misc/singleChat.js
+++ client/src/components/misc/singleChat.js
@@ -10,14 +10,66 @@ import { Input } from "@chakra-ui/react";
 import axios from "axios";
 import { useToast } from "@chakra-ui/react";
 import { useEffect } from "react";
+import { useRef } from "react";
+import io from "socket.io-client";
+
+const ENDPOINT = "http://localhost:3000";
+var socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
   const { selectedChat, user, setSelectedChat } = ChatState();
   const [messages, setMessages] = useState([]);
   const [loading, setLoading] = useState(false);
   const [newMessage, setNewMessage] = useState("");
+  const [socketconnected, setSocketConnected] = useState(false);
+  const [typing, setTyping] = useState(false);
+  const [isTyping, setIsTyping] = useState(false);
+
   const toast = useToast();
   console.log("this is msg", messages);
+  const chatContainerRef = useRef(null);
+
+  useEffect(() => {
+    socket = io(ENDPOINT);
+    socket.emit("setup", user);
+    socket.on("connected", () => {
+      setSocketConnected(true);
+    });
+    socket.on("typing", () => {
+      setIsTyping(true);
+    });
+    socket.on("stop typing", () => {
+      setIsTyping(false);
+    });
+  }, []);
+
+  useEffect(() => {
+    fetchMessages();
+    selectedChatCompare = selectedChat;
+  }, [selectedChat]);
+
+  useEffect(() => {
+    socket.on("message received", (message) => {
+      setMessages([...messages, message]);
+      if (selectedChatCompare._id === message.chat._id) {
+        scrollToBottom();
+      } else {
+        toast({
+          title: "New Message",
+          description: "You have a new message",
+          status: "success",
+          duration: 5000,
+        });
+      }
+    });
+  });
+
+  const scrollToBottom = () => {
+    if (chatContainerRef.current) {
+      chatContainerRef.current.scrollTop =
+        chatContainerRef.current.scrollHeight;
+    }
+  };
 
   const fetchMessages = async () => {
     if (!selectedChat?._id) {
@@ -37,22 +89,21 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
       setMessages(data);
       console.log(data);
       setLoading(false);
+      scrollToBottom();
+      socket.emit("join chat", selectedChat._id);
     } catch (error) {
       toast({
         title: "Error Occured!",
-        description: "Failed to Load the Messages",
+        description: "Failed to Load  the Messages",
         status: "error",
         duration: 5000,
       });
     }
   };
 
-  useEffect(() => {
-    fetchMessages();
-  }, [selectedChat]);
-
   const sendMessage = async (e) => {
     if (e.key === "Enter") {
+      socket.emit("stop typing", selectedChat._id);
       try {
         const config = {
           headers: {
@@ -66,6 +117,7 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
           { content: newMessage, chatID: selectedChat._id },
           config
         );
+        socket.emit("new message", data);
         setMessages([...messages, data]);
       } catch (error) {
         toast({
@@ -80,11 +132,33 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
 
   const typingHandler = (e) => {
     setNewMessage(e.target.value);
-    //typing indicator
+    if (!socketconnected) return;
+
+    if (!typing) {
+      setTyping(true);
+      socket.emit("typing", selectedChat._id);
+    }
+    let lastTyped = new Date().getTime();
+    var timerLength = 3000;
+
+    setTimeout(() => {
+      let currentTime = new Date().getTime();
+      let diff = currentTime - lastTyped;
+      if (diff >= timerLength && typing) {
+        setTyping(false);
+        socket.emit("stop typing", selectedChat._id);
+      }
+    }, timerLength);
   };
 
   const getSenderFull = (loggedUser, users) => {
     return users[0]._id === loggedUser._id ? users[1] : users[0];
+  };
+  const handleBlur = () => {
+    setTyping(false);
+    if (socketconnected) {
+      socket.emit("stop typing", selectedChat._id);
+    }
   };
 
   return (
@@ -102,7 +176,12 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
             alignItems="center"
           >
             <IoMdArrowRoundBack onClick={() => setSelectedChat("")} />
-            <>{selectedChat.chatName.toUpperCase()}</>
+            <div className=" flex items-center">
+              <h1>{selectedChat.chatName.toUpperCase()}</h1>
+              <h1 className=" text-sm ml-4 italic">
+                {isTyping ? "Typing..." : ""}
+              </h1>
+            </div>
             <>
               {!selectedChat.isGroupChat ? (
                 <Profile user={getSenderFull(user, selectedChat.users)} />
@@ -123,33 +202,34 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
             w="100%"
             borderRadius="lg"
             className="h-[70vh] overflow-y-scroll"
+            ref={chatContainerRef}
           >
-            {/* messages here  */}
-            {/* keep the message on the right if the sender is the logged in user and on the left if the sender is the other user */}
-            {messages.map((msg) => (
-              // if the sender is the logged in user then the message will be on the right use only div
-
-              <div
-                key={msg._id}
-                className={`${
-                  msg.sender._id === user._id
-                    ? "flex justify-end"
-                    : "flex justify-start"
-                }`}
-              >
-                <div
-                  className={`${
-                    msg.sender._id === user._id
-                      ? "bg-blue-400 text-white"
-                      : "bg-gray-300 text-black"
-                  } p-2 rounded-lg`}
-                >
-                  {msg.content}
-                </div>
+            {loading ? (
+              <SkeletonChat />
+            ) : (
+              <div>
+                {messages.map((msg) => (
+                  <div
+                    key={msg._id}
+                    className={`${
+                      msg.sender._id === user._id
+                        ? "flex justify-end"
+                        : "flex justify-start"
+                    }`}
+                  >
+                    <div
+                      className={`${
+                        msg.sender._id === user._id
+                          ? "bg-blue-400 text-white"
+                          : "bg-gray-300 text-black"
+                      } p-2 rounded-lg`}
+                    >
+                      {msg.content}
+                    </div>
+                  </div>
+                ))}
               </div>
-            ))}
-
-            {loading ? <SkeletonChat /> : <div>{/* messages  */}</div>}
+            )}
             <FormControl onKeyDown={sendMessage}>
               <Input
                 placeholder="Type a message"
@@ -157,6 +237,7 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
                 bg="white"
                 className=" text-black"
                 value={newMessage}
+                onBlur={handleBlur}
               />
             </FormControl>
           </Box>
--- server/server.js
+++ server/server.js
@@ -20,7 +20,6 @@ app.use(express.json());
 
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
-app.use("/api/messages", messageRoutes);
 app.use("/api/message", messageRoutes);
 
 app.use(notfound);
@@ -28,4 +27,50 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
 console.log("PORT", PORT);
-app.listen(PORT, console.log(`Server started on port ${PORT}`));
+const server = app.listen(PORT, console.log(`Server started on port ${PORT}`));
+
+const io = require("socket.io")(server, {
+  pingTimeout: 50000,
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("New Connection");
+
+  socket.on("setup", (userData) => {
+    socket.join(userData._id); // create user room
+    console.log("User Joined", userData._id);
+    socket.emit("connected");
+  });
+
+  socket.on("join chat", (room) => {
+    socket.join(room);
+    console.log("Room Joined" + room);
+  });
+
+  socket.on("typing", (room) => {
+    socket.in(room).emit("typing");
+  });
+
+  socket.on("stop typing", (room) => {
+    socket.in(room).emit("stop typing");
+  });
+
+  socket.on("new message", (message) => {
+    var chat = message.chat;
+    if (!chat.users) return console.log("chat.users not defined");
+
+    // if we are sending message to a group chat than we will send message to all the users in the group other than the sender
+    chat.users.forEach((user) => {
+      if (user._id == message.sender._id) return;
+      socket.in(user._id).emit("message received", message);
+    });
+  });
+
+  socket.off("setup", () => {
+    console.log("User Disconnected");
+    socket.leave(userData._id);
+  });
+});
